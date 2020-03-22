a825e95b079399b302632164449199d93e1b722a
lukKowalski <krowal@gmail.com>
2020-03-22 22:16:09 +0100

    first version of index.html, publishing generator-tabliczki-mnozenia

---
+++ generator-tabliczki/precache-manifest.be7f3195e04029787157f3cf147303d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "861e65ca0db40fcc8856426d9d245262",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85cc3365020a4d66a2f5",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "72f807a6fbe325fdd992",
+    "url": "/static/css/main.272f1b64.chunk.css"
+  },
+  {
+    "revision": "85cc3365020a4d66a2f5",
+    "url": "/static/js/2.9a2a1b72.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.9a2a1b72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72f807a6fbe325fdd992",
+    "url": "/static/js/main.a1557651.chunk.js"
+  },
+  {
+    "revision": "a1692c7c835683f80c74",
+    "url": "/static/js/runtime-main.2e5e6704.js"
+  }
+]);

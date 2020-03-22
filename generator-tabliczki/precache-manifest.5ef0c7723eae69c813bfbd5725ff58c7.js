8287bc951443af21a36d8fefc352036b2f431de2
lukKowalski <krowal@gmail.com>
2020-03-22 22:54:58 +0100

    updated generator-tabliczki

---
+++ generator-tabliczki/precache-manifest.5ef0c7723eae69c813bfbd5725ff58c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea57fe1ea7c96ea1c95d57e964027684",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e13701f2e049ca45683",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "660cbebe8705de427079",
+    "url": "/static/css/main.3a50df9c.chunk.css"
+  },
+  {
+    "revision": "8e13701f2e049ca45683",
+    "url": "/static/js/2.b35b8f06.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.b35b8f06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "660cbebe8705de427079",
+    "url": "/static/js/main.50422293.chunk.js"
+  },
+  {
+    "revision": "a1692c7c835683f80c74",
+    "url": "/static/js/runtime-main.2e5e6704.js"
+  }
+]);
